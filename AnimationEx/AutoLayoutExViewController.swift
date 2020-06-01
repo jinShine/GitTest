d072b457d15e1d73d92cd1b8eb262ced1a7866bd
jinnify <seungjin429@gmail.com>
2020-06-01 23:21:58 +0900

    add autolayout animation

---
+++ AnimationEx/AutoLayoutExViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AutoLayoutExViewController.swift
+//  AnimationEx
+//
+//  Created by Jinnify on 2020/06/01.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutExViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
